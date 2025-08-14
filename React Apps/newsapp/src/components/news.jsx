8e0cac1a51cfaddaea4bdca12c3ce8b2b81dae04
Ashwaq <ashwaqhussain651@gmail.com>
2025-08-14 20:58:56 +0530

    Added infinite scroll and removed next,previous buttons

--- React Apps/newsapp/src/components/news.jsx
+++ React Apps/newsapp/src/components/news.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Newscomponent from './newscomponent'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class News extends Component {
     static defaultProps = {
@@ -16,64 +17,90 @@ export default class News extends Component {
       category : PropTypes.string,
     }
 
+    capitalizeFirstLetter = (val) => {
+      return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+    };
 
-    constructor(){
-      super();
+
+
+    constructor(props){
+      super(props);
       this.state = {
          articles : [],
          page : 1,
+         totalResults: 0,
+         hasMore: true
       }
+      document.title = `${this.capitalizeFirstLetter(this.props.category)} - News Beacon`
     }
 
+
     async updateNews(){
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c14817eff98a4e74818c03bc54c3bbea&pagesize=${this.props.pageSize}`;
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c14817eff98a4e74818c03bc54c3bbea&page=${this.state.page}&pagesize=${this.props.pageSize}`;
       this.setState({loading : true})
       let promise = await fetch(url);
       let data = await promise.json();
-      this.setState({loading : false})
-      this.setState({articles : data.articles, totalResults: data.totalResults})
+      this.setState({articles : data.articles, totalResults: data.totalResults , loading: false})
     }
 
     async componentDidMount(){
       this.updateNews();
     }
     
-    handlePrevious = async()=>{
-      this.setState({page: this.state.page - 1})
-      this.updateNews();
-    }
+   fetchMoreData = async () => {
+        const nextPage = this.state.page + 1; 
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c14817eff98a4e74818c03bc54c3bbea&page=${nextPage}&pagesize=${this.props.pageSize}`;
 
-    handlenext = async()=>{
-      this.setState({page: this.state.page + 1});
-      this.updateNews();
-      
-    }
+        this.setState({ loading: true });
+        let promise = await fetch(url);
+        let data = await promise.json();
+
+        if(!data.articles || data.articles.length === 0){
+          this.setState({
+            hasMore: false,
+            loading: false,
+          });
+          return
+        }
+
+        this.setState({
+          articles: this.state.articles.concat(data.articles),
+          totalResults: data.totalResults,
+          page: nextPage, 
+          loading: false,
+        });
+      };
+
 
   render() {
     return (
     <>
+  
     <div className="container my-4">
-        <h1 className='text-center'>DAILY NEWS</h1>
-        {this.state.loading && <Spinner></Spinner>}
-        <div className="row">
-            {!this.state.loading && this.state.articles.map((element, index)=>{ 
-              return<div className="col-md-4" key={element.url || index}>
-                    <Newscomponent
-                    title={element.title}
-                    description ={element.description} 
-                    UrlImage={element.urlToImage}
-                    url={element.url}
-                    author={element.author}
-                    date={element.publishedAt}
-                    source={element.source.name}/>
-                    
-                  </div>
-            })}
-        </div>  
-        <div className="container d-flex justify-content-between">
-          <button disabled={this.state.page<=1} type="button" onClick={this.handlePrevious} className="btn btn-dark">&laquo; Previous</button>
-          <button type="button" onClick={this.handlenext} className="btn btn-dark">Next &raquo;</button>
-        </div>
+        <h1 className='text-center'>Top headlines from {this.capitalizeFirstLetter(this.props.category)} Category</h1>
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.hasMore}
+          loader={<Spinner/>}
+        >
+        <div className="Container">
+          <div className="row">
+              {this.state.articles.map((element, index)=>{ 
+                return<div className="col-md-4" key={element.url || index}>
+                      <Newscomponent
+                      title={element.title}
+                      description ={element.description} 
+                      UrlImage={element.urlToImage}
+                      url={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}/>    
+                    </div>
+              })}
+            </div> 
+          </div>
+        </InfiniteScroll> 
     </div>
   </>
 
--- React Apps/newsapp/src/components/newscomponent.jsx
+++ React Apps/newsapp/src/components/newscomponent.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-
+import Placeholder from "../images/Placeholder.png";
 export default class Newscomponent extends Component {
   render() {
     let { title, description, UrlImage, url, author , date, source } = this.props;
+
+    let articlesData = new Date(date);
+
     return (
       <div className="card my-3">
         <span className="position-absolute top-0 translate-middle badge rounded-pill bg-dark" style={{left:'87%', zIndex: 1}}>{source}</span>
-        <img src={UrlImage} className="card-img-top" alt="News" />
+        <img src={UrlImage || Placeholder} className="card-img-top" alt="News" />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{description}</p>
-          <p className="card-text"><small className="text-body-secondary">Last updated by {!author ?"Unkown" :author} at {new Date(date).toUTCString()}</small></p>
+          <p className="card-text"><small className="text-body-secondary">Last updated by {!author ?"Unknown" :author} at {articlesData.toUTCString()}</small></p>
           <a href={url} target="_blank" rel="noreferrer" className="btn btn-primary">
             Read More
           </a>
